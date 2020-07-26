595ed5aac5c89d37862b1f72a2e5fc88423e2f33
rohitmeena12 <rohit.meena@immortal-technologies.com>
2020-07-26 19:04:07 +0530

    Updates

---
+++ precache-manifest.c5de6505ad1ff6a9c0c8c38351b25b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b6fcaa8a600d9093b0065c70745a63",
+    "url": "/react-demo-app/index.html"
+  },
+  {
+    "revision": "d3c949a7941fe2c43c02",
+    "url": "/react-demo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f53b198c1421059b410c",
+    "url": "/react-demo-app/static/js/2.1015ddff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-app/static/js/2.1015ddff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c949a7941fe2c43c02",
+    "url": "/react-demo-app/static/js/main.663e06c3.chunk.js"
+  },
+  {
+    "revision": "58172cd58ee52fd3894c",
+    "url": "/react-demo-app/static/js/runtime-main.37b4c489.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
